58b6706a364b98e88cdde39a7965b8a42148a918
jasmine-g <wm10045@worksmobile.com>
2016-08-01 14:17:22 +0900

    start project

---
+++ js/spec/move.action.test.js
@@ -0,0 +1,26 @@
+
+
+	describe("단일 파일을 이동시키는 경우 1", function() {
+		it("프로그래스바를 띄운다.", function() {
+			expect(true).toBe(true);
+		});
+		it("프로그래스바를 GIF 방식으로 변경한다.", function() {
+			expect(true).toBe(true);
+		});
+		it("용량이 1기가 이상이면 폴링한다.", function() {
+			expect(true).toBe(true);
+		});
+	});
+
+	describe("단일 파일을 이동시키는 경우 2", function() {
+		it("용량이 1기가 미만이면 폴링하지않는다.", function() {
+			expect(true).toBe(true);
+		});
+		it("처리완료 되면 카운트를 변경한다.", function() {
+			expect(true).toBe(true);
+		});
+		it("프로그래스바를 숨긴다.", function() {
+			expect(true).toBe(true);
+		});
+	});
+
